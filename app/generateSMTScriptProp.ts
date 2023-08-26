4207ef9bf01f331178e116578e876d31f643f884
Alex Zlotnik <alex.zlotnik@classdojo.com>
2023-08-26 20:01:09 +0000

    Avoid default exports

--- app/generateSMTScriptProp.ts
+++ app/generateSMTScriptProp.ts
@@ -1,6 +1,6 @@
 interface Smt { smt2: string; propositions: Set<string>; }
 
-export default function generateSMTScriptProp(userSmt: Smt, sysSmt: Smt): string {
+export function generateSMTScriptProp(userSmt: Smt, sysSmt: Smt): string {
   const declarationsArray = Array.from(new Set([...userSmt.propositions, ...sysSmt.propositions])).map((d) => `(declare-const ${d} Bool)`);
   declarationsArray.push(`(assert (not (= ${userSmt.smt2} ${sysSmt.smt2})))`);
   return declarationsArray.join("\n");
