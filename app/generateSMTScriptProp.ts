da1c019422f56684e1de19f8694531d7f05dbec5
Alex Zlotnik <alex.zlotnik@classdojo.com>
2023-09-02 04:49:52 +0000

    I guess types are better

--- app/generateSMTScriptProp.ts
+++ app/generateSMTScriptProp.ts
@@ -1,4 +1,4 @@
-interface Smt { smt2: string; propositions: Set<string>; }
+type Smt = { smt2: string; propositions: Set<string>; };
 
 export function generateSMTScriptProp(userSmt: Smt, sysSmt: Smt): string {
   const declarationsArray = Array.from(new Set([...userSmt.propositions, ...sysSmt.propositions])).map((d) => `(declare-const ${d} Bool)`);
